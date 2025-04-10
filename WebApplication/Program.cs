5e68ce495fe3e8eff1465ef57be53086bf9fce46
Daniel <daniel3204155185@gmail.com>
2025-04-09 21:44:50 -0500

    solucionado

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -2,51 +2,45 @@ using Bussines;
 using Data;
 using Entity.Context;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
 
-internal class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Registrar clases de Rol
+builder.Services.AddScoped<RoleData>();
+builder.Services.AddScoped<RoleBusiness>();
+
+
+// Agregar CORS
+var OrigenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(",");
+builder.Services.AddCors(opciones =>
 {
-    private static void Main(string[] args)
+    opciones.AddDefaultPolicy(politica =>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        politica.WithOrigins(OrigenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
-        // Add services to the container.
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+// Agregar DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+    opciones.UseSqlServer("name=DefaultConnection"));
 
+var app = builder.Build();
 
-        // Registrar clases de Rol
-        builder.Services.AddScoped<RoleData>();
-        builder.Services.AddScoped<RoleBusiness>();
-
-
-        // Agregar CORS
-        var OrigenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(",");
-        builder.Services.AddCors(opciones =>
-        {
-            opciones.AddDefaultPolicy(politica =>
-            {
-                politica.WithOrigins(OrigenesPermitidos).AllowAnyHeader().AllowAnyMethod();
-            });
-        });
-
-        // Agregar DbContext
-        builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
-            opciones.UseSqlServer("name=DefaultConnection"));
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();

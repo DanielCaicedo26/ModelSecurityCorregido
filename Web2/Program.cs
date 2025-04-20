389f91803edf3a099cd86f907a6809811522945a
Daniel <daniel3204155185@gmail.com>
2025-04-20 17:16:07 -0500

    program

--- Web2/Program.cs
+++ Web2/Program.cs
@@ -72,6 +72,14 @@ builder.Services.AddCors(opciones =>
     });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy.WithOrigins("http://127.0.0.1:5500")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
 
 
 var app = builder.Build();
